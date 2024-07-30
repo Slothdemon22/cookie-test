eb1126a47464ab4d32b8c2989ace8b61583825f4
Slothdemon22 <162999948+Slothdemon22@users.noreply.github.com>
2024-07-30 13:10:54 +0500

    cookie-test

--- src/app/api/genCookie/route.ts
+++ src/app/api/genCookie/route.ts
@@ -1,13 +1,23 @@
 import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+export async function POST(req: NextRequest) {
+//   const cookieValue = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 
-export async function GET() {
-  const cookieValue = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+//   const cookieStore = cookies();
+//   cookieStore.set('token', cookieValue, { path: '/', httpOnly: true, secure: true, sameSite: 'strict' });
 
-  const cookieStore = cookies();
-  cookieStore.set('token', cookieValue, { path: '/', httpOnly: true, secure: true, sameSite: 'strict' });
+//   return new Response(JSON.stringify({ message: 'Cookie has been set' }), {
+//     status: 200,
+//     headers: { 'Content-Type': 'application/json' },
+    //   });
+    
 
-  return new Response(JSON.stringify({ message: 'Cookie has been set' }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-  });
+    const reqBody=await req.json();
+    console.log(reqBody);
+    console.log(reqBody.name);
+    const cookieStore = cookies();
+    cookieStore.set("Token", reqBody.name, { path: "/", httpOnly: true, secure: true, sameSite: "strict" });
+    return NextResponse.json({ Msg: "cookie created Successfully" });
+
+
 }
--- src/app/cookie/page.tsx
+++ src/app/cookie/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 import axios from "axios";
+import { useState } from "react";
 export default function Cookie() {
+    const [Name, setName] = useState("");
 
     const handleCookie = async() => {
 
-        const res = await axios.get('/api/genCookie');
+        const res = await axios.post('/api/genCookie', { name: Name });
 
         console.log(res)
      
@@ -13,8 +15,10 @@ export default function Cookie() {
 
 
     return (
-        <div className="flex min-h-screen  justify-center items-center  p-24 bg-slate-600">
-            <h1 onClick={handleCookie} className="text-3xl cursor-pointer text-white bg-slate-800 p-6 rounded-2xl">Cookie</h1>
+        <div className="flex flex-col min-h-screen  justify-center items-center  p-24 bg-slate-600">
+            <label htmlFor="name" className="p-2 m-2">Name</label>
+            <input type="text" className="p-2 px-2 m-2 rounded-2xl" placeholder="Name" value={Name} onChange={(e) => setName(e.target.value)} id="name" />
+            <h1 onClick={handleCookie} className="text-3xl cursor-pointer text-white bg-slate-800 p-4 rounded-2xl">Cookie</h1>
         </div>
     );
 }
